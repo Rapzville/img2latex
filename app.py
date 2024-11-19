00138fc334d747c3497e5d6c4879a95e999c171e
Rapzville <artchihladze05@gmail.com>
2024-11-19 09:57:17 +0300

    add_return_result

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
 from flask import Flask, render_template, url_for, redirect, request, flash
 from werkzeug.utils import secure_filename
-from model_predict import img2latex
+from model import predict
 
-# папка для сохранения загруженных файлов
+
 UPLOAD_FOLDER = '/uploaded/'
-# расширения файлов, которые разрешено загружать
+
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'webp'}
 
 app = Flask(__name__)
@@ -13,46 +13,39 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
-    """ Функция проверки расширения файла """
+    
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-with app.test_request_context():
-    pass
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        # проверим, передается ли в запросе файл 
-
-        if 'file' not in request.files:
-            # После перенаправления на страницу загрузки
-            # покажем сообщение пользователю 
-            flash('Файл не найден в запросе!')
+        if 'file' not in request.files:    
             return redirect(request.url)
+
         file = request.files['file']
+
         # Если файл не выбран, то браузер может
         # отправить пустой файл без имени.
         if file.filename == '':
             flash('Нет выбранного файла')
             return redirect(request.url)
+        
         if file and allowed_file(file.filename):
-            # безопасно извлекаем оригинальное имя файла
+
             filename = secure_filename(file.filename)
-            # сохраняем файл
+            
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename)[1:])
-            # если все прошло успешно, то перенаправляем  
-            # на функцию-представление `download_file` 
-            # для скачивания файла
-            with app.test_request_context():
-                print(os.path.join(app.config['UPLOAD_FOLDER'], filename)[1:])
-            return redirect(url_for('result', filename=filename))
+            
+            return redirect(url_for('uploaded', filename=filename))
     
     return render_template('index.html')
 
-@app.route('/result/<filename>')
-def download_file(filename):
-    return f'Функционал загрузки файла {filename} еще не реализован.'
+@app.route('/uploaded/<filename>')
+def uploaded(filename):
+    res = predict(f'uploaded/{filename}')
+    os.remove(f'uploaded/{filename}')
+    return f'{res}'
 
 if __name__ == '__main__':
     app.config['SECRET_KEY'] = 'secret'
--- model.py
+++ model.py
@@ -1,40 +1,10 @@
-import onnxruntime as rt
-import numpy as np
-import cv2
-from tqdm import tqdm
-import shutil
-import json
+from PIL import Image
+from pix2tex.cli import LatexOCR
 
-class RecognitionModel:
-    def __init__(self):
-        providers = ['CPUExecutionProvider']
-        self.model = rt.InferenceSession('./model.onnx', providers=providers)
-        self.output_name = self.model.get_outputs()[0].name
-        self.input_name = self.model.get_inputs()[0].name
-        self.ltx_index = json.load((open('keys.json')))
-
-    def predict(self, img_path, w, h):
-        img = self.load(img_path, w, h)
-        res = self.model.run([self.output_name], {self.input_name: img})[0][0]
-        res = np.argmax(res, axis=1)
-
-        l = ''.join([self.ltx_index[str(x - 1)] if x != 0 else ' ' for x in res])
-
-        return l
-    
-    def resize(self, img, w=256, h=256):
-        p = max(img.shape[:2] / np.array([h, w]))
-        s = img.shape[:2]
-        r = s / p
+def predict(img_path):
+    model = LatexOCR()
+    img = Image.open(img_path)
         
-        img = cv2.resize(img, (int(r[1]), int(r[0])))
+    res = model(img)
 
-        re = np.zeros((h, w, 3))
-        offset = np.array((np.array(re.shape[:2]) - np.array(img.shape[:2])) / 2, dtype=np.int32)
-        re[offset[0]:offset[0] + img.shape[0], offset[1]:offset[1] + img.shape[1]] = img
-        return re
-
-    def load(self, image_file, w, h):
-        i = cv2.imread(image_file)[...,::-1]
-        input_image = self.resize(i, w, h) / 255
-        return input_image[None].astype(np.float32)
+    return res
